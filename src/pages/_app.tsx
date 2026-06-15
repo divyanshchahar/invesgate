317c3a94bf5e387c6210561380d78f1bb4115147
divyanshchahar <divyanshchahar@outlook.com>
2026-06-15 09:36:42 +0530

    add:added global fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {Playfair_Display, Source_Sans_3} from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const straightFont = Source_Sans_3({
+    variable: "--font-family-straight"
+})
+
+const cursiveFont = Playfair_Display({
+    variable: "--font-family-cursive"
+})
+
+export default function App({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />;
 }
