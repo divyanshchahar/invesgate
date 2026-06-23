968673144a801b66ab22ce5553688d49df155662
divyanshchahar <divyanshchahar@outlook.com>
2026-06-23 14:10:49 +0530

    add:added new layout

---
+++ src/ui/layouts/HeadingLayout.tsx
@@ -0,0 +1,25 @@
+import styles from "./HeadingLayout.module.css"
+import {ReactElement} from "react";
+
+export interface HeadingSectionLayoutPropTypes {
+    colorScheme: string,
+    heading: ReactElement,
+    accentText: string,
+    textAlignment: "center" | "left",
+    styling?: string
+}
+
+export default function HeadingLayout({
+                                          accentText,
+                                          heading,
+                                          textAlignment,
+                                          styling,
+                                          colorScheme
+                                      }: HeadingSectionLayoutPropTypes) {
+    return (
+        <div className={`${colorScheme} ${styling} ${textAlignment === "center" ? styles.center : styles.left} `}>
+            <p className={styles.accentText}>{accentText}</p>
+            <div className={styles.headingText}>{heading}</div>
+        </div>
+    )
+}
