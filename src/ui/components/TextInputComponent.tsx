9d49a7bdf7af71d6dc19a54058277a5bd76afa4a
divyanshchahar <divyanshchahar@outlook.com>
2026-06-26 17:17:08 +0530

    commented temporarily

--- src/ui/components/TextInputComponent.tsx
+++ src/ui/components/TextInputComponent.tsx
@@ -1,16 +1,16 @@
-import styles from "./TextInputComponent.module.css"
-
-export interface TextInputComponentPropTypes<T> {
-    fieldName: string,
-    colorScheme: string;
-    props?: T;
-}
-
-export default function CTAButton({fieldName, colorScheme, ...props}: TextInputComponentPropTypes<T>) {
-    return (
-        <div className={`${colorScheme} ${styles.container}`}>
-            <label>{fieldName}</label>
-            <br/>
-            <input type="text" {...props} />
-        </div>)
-}
+// import styles from "./TextInputComponent.module.css"
+//
+// export interface TextInputComponentPropTypes<T> {
+//     fieldName: string,
+//     colorScheme: string;
+//     props?: T;
+// }
+//
+// export default function CTAButton({fieldName, colorScheme, ...props}: TextInputComponentPropTypes<T>) {
+//     return (
+//         <div className={`${colorScheme} ${styles.container}`}>
+//             <label>{fieldName}</label>
+//             <br/>
+//             <input type="text" {...props} />
+//         </div>)
+// }
